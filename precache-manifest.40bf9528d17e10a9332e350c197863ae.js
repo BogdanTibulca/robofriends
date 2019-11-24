53d2a1d10aae36175d4a3108083c42dc69080036
Bogdan Tibulca <bogdantibulca@yahoo.com>
2019-11-24 13:39:06 +0200

    Updates

---
+++ precache-manifest.40bf9528d17e10a9332e350c197863ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879c9b2674d4713856d823ae597faeca",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e57dff58f8c5732f81b8",
+    "url": "/robofriends/static/css/main.7bb9a453.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "e57dff58f8c5732f81b8",
+    "url": "/robofriends/static/js/main.f9c93d01.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
